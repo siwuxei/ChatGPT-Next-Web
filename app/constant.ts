d4e6d42e55b90fbc67f50fdfae3fd77c4118cb61
Li Lele <50586809+siwuxei@users.noreply.github.com>
2024-05-10 13:10:17 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -158,9 +158,9 @@ const anthropicModels = [
   "claude-instant-1.2",
   "claude-2.0",
   "claude-2.1",
-  "claude-3-sonnet-20240229",
-  "claude-3-opus-20240229",
-  "claude-3-haiku-20240307",
+  // "claude-3-sonnet-20240229",
+  // "claude-3-opus-20240229",
+  // "claude-3-haiku-20240307",
 ];
 
 export const DEFAULT_MODELS = [
